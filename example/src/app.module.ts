66945bf76f819c5dc44d7cb22d5e8f69aa07698e
joshunrau <contact@joshuaunrau.com>
2023-09-19 18:20:42 -0400

    add config

--- example/src/app.module.ts
+++ example/src/app.module.ts
@@ -1,10 +1,16 @@
 import { Module, ValidationPipe } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
 import { APP_PIPE } from '@nestjs/core';
 
 import { CatsModule } from './cats/cats.module';
 
 @Module({
-  imports: [CatsModule],
+  imports: [
+    CatsModule,
+    ConfigModule.forRoot({
+      isGlobal: true
+    })
+  ],
   providers: [
     {
       provide: APP_PIPE,
--- example/src/main.ts
+++ example/src/main.ts
@@ -1,13 +1,15 @@
+import { ConfigService } from '@nestjs/config';
 import { NestFactory } from '@nestjs/core';
 
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  const port = 3000;
+  const configService = app.get(ConfigService);
+
+  const port = parseInt(configService.getOrThrow<string>('PORT'));
   await app.listen(port);
   console.log(`Application is running on: ${await app.getUrl()}`);
 }
 
 void bootstrap();
-                                                                                       
