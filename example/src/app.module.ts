7d036c4c94078147e7b9546e4366c5d2ca86a2a9
joshunrau <contact@joshuaunrau.com>
2023-09-19 14:47:31 -0400

    add injection

--- example/src/app.module.ts
+++ example/src/app.module.ts
@@ -1,8 +1,8 @@
 import { Module } from '@nestjs/common';
 
-import { CatsController } from './cats/cats.controller';
+import { CatsModule } from './cats/cats.module';
 
 @Module({
-  controllers: [CatsController]
+  imports: [CatsModule]
 })
 export class AppModule {}
