34abad3e997070f4578496ece6d43f58778c8e64
joshunrau <contact@joshuaunrau.com>
2023-09-19 15:05:27 -0400

    fix script

--- example/scripts/build.ts
+++ example/scripts/build.ts
@@ -3,7 +3,7 @@ import path from 'path';
 import swcPlugin from 'bun-plugin-swc';
 
 const result = await Bun.build({
-  entrypoints: [path.resolve(import.meta.dir, 'src', 'main.ts')],
+  entrypoints: [path.resolve(import.meta.dir, '..', 'src', 'main.ts')],
   external: ['@nestjs/microservices', '@nestjs/websockets/socket-module'],
   outdir: path.resolve(import.meta.dir, 'dist'),
   plugins: [swcPlugin()],
