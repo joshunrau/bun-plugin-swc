18a600340c4d23f7910e9c3c0e699ba0f7b9f170
joshunrau <contact@joshuaunrau.com>
2023-09-19 15:06:52 -0400

    rm superfluous package

--- example/src/main.ts
+++ example/src/main.ts
@@ -1,10 +1,9 @@
 import { NestFactory } from '@nestjs/core';
-import { NestExpressApplication } from '@nestjs/platform-express';
 
 import { AppModule } from './app.module';
 
 async function bootstrap() {
-  const app = await NestFactory.create<NestExpressApplication>(AppModule);
+  const app = await NestFactory.create(AppModule);
   const port = 3000;
   await app.listen(port);
   console.log(`Application is running on: ${await app.getUrl()}`);
