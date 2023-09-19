e67857a13db020d8fa2ef9493d676251e697dd86
joshunrau <contact@joshuaunrau.com>
2023-09-19 13:31:17 -0400

    works

---
+++ src/index.ts
@@ -0,0 +1,35 @@
+import swc from '@swc/core';
+import { type BunPlugin } from 'bun';
+
+const plugin: BunPlugin = {
+  name: 'bun-plugin-swc',
+  setup: (build) => {
+    build.onLoad({ filter: /\.ts$/ }, async (args) => {
+      const output = await swc.transformFile(args.path, {
+        jsc: {
+          keepClassNames: true,
+          parser: {
+            decorators: true,
+            dynamicImport: true,
+            syntax: 'typescript'
+          },
+          preserveAllComments: true,
+          target: 'esnext',
+          transform: {
+            decoratorMetadata: true,
+            legacyDecorator: true
+          }
+        },
+        module: {
+          type: 'es6'
+        }
+      });
+      return {
+        contents: output.code,
+        loader: 'js'
+      };
+    });
+  }
+};
+
+export default plugin;
